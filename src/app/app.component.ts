0091af6209b56f826911362542825866c0b6c932
Willy TCHANA <willytchana@live.fr>
2020-04-16 13:11:41 +0100

    Fix fonts, favicon & page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  appName = "MyBiblio"
   title = 'biblioIUC';
+  constructor(private pageTitle: Title){
+  
+  }
+  ngOnInit(){
+    this.pageTitle.setTitle(this.appName + " - Dashboard");
+  }
 }
