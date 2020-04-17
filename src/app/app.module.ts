3efc45fb90b646bb032a66664f740549ef321eeb
Jessica <jessicadomez2005@yahoo.fr>
2020-04-17 12:26:08 +0100

    creation de la page categorie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CategorieEditComponent } from './pages/categorie-edit/categorie-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    CategorieEditComponent
   ],
   imports: [
     BrowserModule,
